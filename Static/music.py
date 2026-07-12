50271b360a590abe6b86d475683067ca188668e3
mdpella41020-hub <mdpella41020@gmail.com>
2026-07-12 19:20:35 +0600

    Add files via upload

---
+++ Static/music.py
@@ -0,0 +1,3 @@
+# Music configuration
+# Direct URL from Catbox.moe - works on all servers including Render
+MUSIC_FILE_PATH = "https://files.catbox.moe/ua29li.mp4"
---
+++ Static/pic.py
@@ -0,0 +1,3 @@
+# Profile picture path configuration
+# Direct URL from ImgBB - works on all servers including Render
+PROFILE_PIC_PATH = "https://i.ibb.co.com/p6xCCh55/IMG-20260701-185837.jpg"
